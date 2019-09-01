643939a1f39bcb6c9457fcdd87c0c1ee99dc94b3
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-09-01 09:58:27 +0200

    Update

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from 'contentful';
 
 export default createClient({
-    space: "799tadt8j6xv",
-    accessToken: "pagLRbOvNRbvCyjpbY_BcC6WGNjonWYX7td8exWTohQ"
-})
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
+});
